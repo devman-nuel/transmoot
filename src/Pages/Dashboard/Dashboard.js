84f99d1a3b59e2b05bc26c2fc7fef83ce0cdb509
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2024-04-15 21:15:06 +0100

    yes

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -64,9 +64,84 @@ function Dashboard() {
              <p>10gb used</p>
            </div>
         </div>
-
       </div>
- 
+
+      <div className='sub-dashboard'>
+        <div className='work-history'>
+          <div className='work-header'>
+            <p>Work History</p>
+
+            <button>
+               <p>All projects</p>
+               <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1713210865/icons8-right-arrow-50_jl72wg.png' alt=''></img>
+            </button>
+          </div>
+
+          <div className='work-list'>
+             <span>No projects yet</span>
+          </div>         
+        </div>
+
+        <div className='resource'>
+          <div className='resource-header'>
+              <p>Resource</p>
+           
+              <img src='' alt=''></img>
+            </div>
+
+            <div className='resource-list'>
+              <div className='resource-box'>
+                <div className='resource-text'>
+                  <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1713173706/wave-sphere_nhsvdd.png' alt=''></img> 
+                  <div className='resource-main'>
+                    <p>Lantern Starter</p>
+                    <span>Free templates with plugins and tools to create from </span>
+                  </div>
+                </div>
+
+                <img className='resource-img' src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1700718884/icons8-dropdown-50_nfgubs.png' alt=''></img>           
+              </div>
+              
+              <div className='resource-box'>
+                <div className='resource-text'>
+                  <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1713173706/wave-sphere_nhsvdd.png' alt=''></img> 
+                  <div className='resource-main'>
+                    <p>Turbo Starter</p>
+                    <span>Free templates with plugins and tools to create from </span>
+                  </div>
+                </div>
+
+                <img className='resource-img' src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1700718884/icons8-dropdown-50_nfgubs.png' alt=''></img>           
+              </div>
+
+              <div className='resource-box'>
+                <div className='resource-text'>
+                  <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1713173706/wave-sphere_nhsvdd.png' alt=''></img> 
+                  <div className='resource-main'>
+                    <p>Swelter</p>
+                    <span>Free templates with plugins and tools to create from </span>
+                  </div>
+                </div>
+
+                <img className='resource-img' src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1700718884/icons8-dropdown-50_nfgubs.png' alt=''></img>           
+              </div>
+
+              <div className='resource-box'>
+                <div className='resource-text'>
+                  <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1713173706/wave-sphere_nhsvdd.png' alt=''></img> 
+                  <div className='resource-main'>
+                    <p>Charlmeme</p>
+                    <span>Free templates with plugins and tools to create from </span>
+                  </div>
+                </div>
+
+                <img className='resource-img' src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1700718884/icons8-dropdown-50_nfgubs.png' alt=''></img>           
+              </div>
+
+            </div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,14 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './LogIn.css';
 
 const LogIn = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  // Function to handle click on pagination dots or eclipse
+  const handleSlideChange = (index) => {
+    setActiveSlide(index);
+  };
+
+  // Function to handle click on eclipse to move slider forward
+  const handleNextSlide = () => {
+    setActiveSlide((prevSlide) => (prevSlide === 3 ? 0 : prevSlide + 1));
+  };
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveSlide((prevSlide) => (prevSlide === 3 ? 0 : prevSlide + 1));
+    }, 5000); // Change interval duration as per your preference (5000ms = 5 seconds)
+
+    return () => clearInterval(interval);
+  }, []); // Empty dependency array ensures that effect runs only once, similar to componentDidMount
 
 
   return (
     <div className='auth-con'>
       <div className='auth-left'>
-        <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1712796905/9bfa7b67994815.5b4dc2c29ce03_ovvuwm.jpg' alt='' />
+      <div className='team-img'>
+          <div className='team-wrap'>
+            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1713166582/Group_2241_goz0w3.png' alt='' />
+          </div>
+        </div>
+
+        <div className='slide-con'>
+          <div className='slider'>
+            <div className='slide-text'>
+              {/* Array of slide texts */}
+              {['Create', 'Discover', 'Connect', 'Achieve'].map((text, index) => (
+                <div key={index} className={`slide ${index === activeSlide ? 'active' : ''}`}>
+                  <p>{text}</p>
+                  <span>
+                    {/* Add corresponding description texts here */}
+                    {index === 0 && "Unleash your creative potential and bring your vision to life with Transmoot. Dive into a world of endless possibilities as you craft captivating visuals with ease and precision."}
+                    {index === 1 && "Discover the power of Transmoot and unlock new dimensions of creativity. Explore innovative tools and features designed to elevate your projects to new heights."}
+                    {index === 2 && "Connect with a community of like-minded creators and collaborators. Share your work, exchange ideas, and forge meaningful connections that propel your creative journey forward."}
+                    {index === 3 && "Achieve your goals and turn your dreams into reality with Transmoot. Whether you're a beginner or a seasoned pro, our platform empowers you to achieve success on your terms."}
+                  </span>
+                </div>
+              ))}
+            </div>
+
+            <div className='navigation'>
+              <div className='pagination'>
+                {/* Map through slides and create pagination dots */}
+                {[...Array(4)].map((_, index) => (
+                  <div
+                    key={index}
+                    className={`slid ${index === activeSlide ? 'active' : ''}`}
+                    onClick={() => handleSlideChange(index)}
+                  />
+                ))}
+              </div>
+
+              <div className='eclipse' onClick={handleNextSlide}>
+                <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1700718884/icons8-dropdown-50_nfgubs.png' alt='' />
+              </div>
+            </div>
+          </div>
+        </div>
+       
       </div>
 
       <div className='auth-right'>
